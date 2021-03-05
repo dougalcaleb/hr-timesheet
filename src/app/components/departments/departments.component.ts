6c772630a40e8d6cb40932095684a20782f8002c
dougalcaleb <dougalcaleb@gmail.com>
2021-03-05 10:23:15 -0700

    partway through 6

---
+++ src/app/components/departments/departments.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Department } from 'src/app/interfaces/department';
+import { DepartmentsService } from 'src/app/services/departments.service';
+
+@Component({
+  selector: 'app-departments',
+  templateUrl: './departments.component.html',
+  styleUrls: ['./departments.component.scss']
+})
+export class DepartmentsComponent implements OnInit {
+
+   departments: Department[] = [];
+
+   constructor(private departmentsService: DepartmentsService, private router: Router) {
+     
+  }
+
+   goToDepartment(departmentId: string): void {
+      this.router.navigate(["./timesheet", { id: departmentId }]);
+   }
+   ngOnInit(): void {
+      this.departments = this.departmentsService.departments;
+  }
+}
