275142b788ec0a2f15565d4a64c107fc1fa8f3e3
dougalcaleb <dougalcaleb@gmail.com>
2021-03-08 09:26:14 -0700

    through module 6

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Department } from 'src/app/interfaces/department';
+import { DepartmentsService } from 'src/app/services/departments.service';
 
 @Component({
-  selector: 'app-timesheet',
-  templateUrl: './timesheet.component.html',
-  styleUrls: ['./timesheet.component.scss']
+   selector: 'app-timesheet',
+   templateUrl: './timesheet.component.html',
+   styleUrls: ['./timesheet.component.scss']
 })
 export class TimesheetComponent implements OnInit {
+   departments: Department[] = [];
+   department: any;
 
-  constructor() { }
+   constructor(
+      private route: ActivatedRoute, private departmetsService: DepartmentsService
+   ) { }
 
-  ngOnInit(): void {
-  }
-
+   ngOnInit(): void {
+      this.departments = this.departmetsService.departments;
+      this.department = this.departments.find(d => d.id === this.route.snapshot.params["id"]);
+   }
 }
